db4bf3c24fe2c9d953ac52f66470b5354f0812a3
GusevADresume <resumeGusevAD@gmail.com>
2022-04-21 00:58:53 +0300

    fin

--- advertisements/filters.py
+++ advertisements/filters.py
@@ -7,7 +7,8 @@ class AdvertisementFilter(filters.FilterSet):
     """Фильтры для объявлений."""
     creator = filters.CharFilter(field_name='creator_id', lookup_expr='in')
     created_at = DateFromToRangeFilter(field_name='created_at', lookup_expr='in')
+    status = filters.CharFilter(field_name='status')
 
     class Meta:
         model = Advertisement
-        fields = ['creator', 'created_at']
+        fields = ['creator', 'created_at', 'status']
--- advertisements/views.py
+++ advertisements/views.py
@@ -5,16 +5,10 @@ from rest_framework.viewsets import ModelViewSet
 from advertisements.filters import AdvertisementFilter
 from advertisements.models import Advertisement
 from advertisements.serializers import AdvertisementSerializer
+from api_with_restrictions.permissioms import IsOwner
 
 
-class DestroyAdv(permissions.BasePermission):
-    def destroy(self, request, pk=None):
-        if Advertisement.objects.filter(id=request.parser_context['kwargs']['pk']).values()[0][
-            'creator_id'] == request.user.id:
-            return [IsAuthenticated()]
-        else:
-            raise ValidationError("it is not you'r adv")
-        return response
+
 
 
 class AdvertisementViewSet(ModelViewSet):
@@ -23,17 +17,7 @@ class AdvertisementViewSet(ModelViewSet):
     filterset_class = AdvertisementFilter
 
     def get_permissions(self):
-        if self.action in ["update", "partial_update"]:
-            if Advertisement.objects.filter(id=self.request.parser_context['kwargs']['pk']).values()[0][
-                'creator_id'] == self.request.user.id:
-                return [IsAuthenticated()]
-            else:
-                raise ValidationError("it is not you'r adv")
-        if self.action in ['destroy']:
-            d = DestroyAdv()
-            d.destroy(request=self.request, pk=self.request.parser_context['kwargs']['pk'])
+        print(self.action)
+        if self.action in ["create", "update", "partial_update","destroy"]:
+            return [IsAuthenticated(), IsOwner()]
         return []
-
-
-
-
